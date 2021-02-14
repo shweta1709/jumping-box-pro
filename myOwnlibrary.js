0eeec4a8e2a5251baa4aeba698991320274aa941
shweta1709 <76553502+shweta1709@users.noreply.github.com>
2021-02-14 17:58:11 +0530

    Add files via upload

---
+++ myOwnlibrary.js
@@ -0,0 +1,80 @@
+function isTouching(){
+
+    if(surface1.isTouching(box) ){
+        box.shapeColor = "red";
+        music.play();
+
+    }
+ 
+    if(surface2.isTouching(box) ){
+     box.shapeColor = "red";
+     
+ }
+ 
+ if(surface3.isTouching(box) ){
+     box.shapeColor = "red";
+     box.velocityX =0;
+     box.velocityY =0;
+     music.play();
+     
+ }
+ 
+ if(surface4.isTouching(box) ){
+     box.shapeColor = "red";
+    
+
+    
+ }
+ 
+
+}
+
+function bounceOff(){
+
+    if(box.bounceOff(syrface1) ){
+        box.shapeColor = "red";
+        music.play();
+        
+    }
+    
+    if(box.bounceOff(surface2) ){
+        box.shapeColor = "blue";
+        
+        
+    }
+    
+    if(box.bounceOff(surface3) ){
+        box.shapeColor = "orange";
+       
+        
+    }
+    if(box.bounceOff(surface4) ){
+        box.shapeColor ="yellow" ;
+      
+        
+    }
+        
+    
+    box.bounceOff(edge1);
+   
+
+    box.bounceOff(edge2);
+    
+    
+
+    box.bounceOff(edge3);
+    
+
+    box.bounceOff(edge4);
+    
+
+
+}
+  
+
+
+
+
+    
+
+
